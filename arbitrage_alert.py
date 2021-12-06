8c2f845fe9aeeead6f75f8578f2ca39c30fb9b7e
laptop <eric@volz.link>
2021-12-06 17:59:22 +0100

    finsished arbitrage script

---
+++ arbitrage_alert.py
@@ -0,0 +1,57 @@
+import TelegramBot as tb
+import CryptoAPIs as ca
+import time
+import datetime as dt
+
+
+class ArbitrageAlert:
+    def __init__(self, exchange_1, exchange_2, pair_1, pair_2, arbitrage_rate, interval, telegram_token):
+        self.exchange_1_name = exchange_1
+        self.exchange_2_name = exchange_2
+        self.exchange_1 = ca.get_exchange(exchange_1)
+        self.exchange_2 = ca.get_exchange(exchange_2)
+        self.pair_1 = pair_1
+        self.pair_2 = pair_2
+        self.arbitrage_rate = arbitrage_rate
+        self.interval = interval
+        self.suspend_until = None
+        self.telegram_token = telegram_token
+        self.telegram_chat_id = None
+        try:
+            self.telegram_chat_id = tb.TelegramBot.getChatID(self.telegram_token)
+        except Exception as e:
+            raise e
+        self.handle()
+
+    def handle(self):
+        bot = tb.TelegramBot(self.telegram_token, self.telegram_chat_id)
+        while True:
+            try:
+                price_1 = self.exchange_1.get_price(self.pair_1)
+                price_2 = self.exchange_2.get_price(self.pair_2)
+
+                price_difference = round(price_1 - price_2, 2)
+                percent_difference = round(1 - price_2 / price_1, 2)
+
+                print(percent_difference)
+
+                if percent_difference >= self.arbitrage_rate:
+                    if not self.suspend_until:
+                        self.suspend_until = dt.datetime.utcnow() + dt.timedelta(seconds=self.interval)
+                        msg = f"Arbitrage Möglichkeit zwischen:\n " \
+                              f"{self.exchange_1_name} = {price_1}$\n" \
+                              f"{self.exchange_2_name} = {price_2}$\n" \
+                              f"{price_difference}$ - {percent_difference * 100}%"
+                        bot.sendMessage(msg)
+
+                if self.suspend_until <= dt.datetime.utcnow():
+                    self.suspend_until = None
+
+                time.sleep(1)
+            except Exception as e:
+                print(e)
+                print("Wird neu gestartet...")
+
+if '__main__' == __name__:
+    token = "1910759993:AAGpzGdVPm3af7ld0VfmGKQEDncRFuk-fTA"
+    obj = ArbitrageAlert('kucoin', 'defichain_dex', "DFI-USDT", "USDC-DFI", 0.01, 5, token)
